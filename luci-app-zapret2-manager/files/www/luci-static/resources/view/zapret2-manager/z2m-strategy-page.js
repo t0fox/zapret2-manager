8dcdd86c9f31ffb00d007d6a2e551abe4db39318
Kirill <90920135+t0fox@users.noreply.github.com>
2026-08-03 20:23:38 +0300

    feat: compose active Orchestra runs into Strategy

--- luci-app-zapret2-manager/files/www/luci-static/resources/view/zapret2-manager/z2m-strategy-page.js
+++ luci-app-zapret2-manager/files/www/luci-static/resources/view/zapret2-manager/z2m-strategy-page.js
@@ -1,6 +1,7 @@
 'use strict';
 'require view.zapret2-manager.z2m-strategy as Strategy';
 'require view.zapret2-manager.z2m-auto as Auto';
+'require view.zapret2-manager.z2m-runs as Runs';
 
 function strategyContext(ctx, data, root) {
   return Object.assign({}, ctx, { data: data || {}, root: root || ctx.root });
@@ -8,20 +9,24 @@ function strategyContext(ctx, data, root) {
 function load(ctx) {
   return Promise.all([
     Strategy.load(ctx),
-    Auto.load(ctx)
+    Auto.load(ctx),
+    Runs.load(ctx)
   ]).then(function (results) {
-    return { strategy: results[0] || {}, auto: results[1] || {} };
+    return { strategy: results[0] || {}, auto: results[1] || {}, runs: results[2] || {} };
   });
 }
 function render(ctx) {
   var coreCtx = strategyContext(ctx, ctx.data && ctx.data.strategy);
   var node = Strategy.render(coreCtx);
   var autoPanel = Auto.render(ctx, ctx.data && ctx.data.auto);
+  var runsPanel = Runs.render(ctx, ctx.data && ctx.data.runs);
   if (node && node.insertBefore) {
     var before = node.querySelector && node.querySelector('.z2m-subtabs');
     node.insertBefore(autoPanel, before || null);
+    node.insertBefore(runsPanel, before || null);
   } else if (node && node.appendChild) {
     node.appendChild(autoPanel);
+    node.appendChild(runsPanel);
   }
   return node;
 }
@@ -31,6 +36,7 @@ function mount(ctx) {
 function unmount(ctx) {
   Strategy.unmount(strategyContext(ctx, ctx && ctx.data && ctx.data.strategy, ctx && ctx.root));
   Auto.unmount();
+  Runs.unmount();
 }
 
 return {
