2c5bfcd44cb9beec48958dd635f7cb2427449750
Kirill <90920135+t0fox@users.noreply.github.com>
2026-08-04 23:02:53 +0300

    feat: make Strategy the single owner of its reference workflow

--- luci-app-zapret2-manager/files/www/luci-static/resources/view/zapret2-manager/z2m-strategy-page.js
+++ luci-app-zapret2-manager/files/www/luci-static/resources/view/zapret2-manager/z2m-strategy-page.js
@@ -1,50 +1,27 @@
 'use strict';
 'require baseclass';
 'require view.zapret2-manager.z2m-strategy as Strategy';
-'require view.zapret2-manager.z2m-auto as Auto';
-'require view.zapret2-manager.z2m-runs as Runs';
 
-function strategyContext(ctx, data, root) {
-  return Object.assign({}, ctx, { data: data || {}, root: root || ctx.root });
-}
 function load(ctx) {
-  return Promise.all([
-    Strategy.load(ctx),
-    Auto.load(ctx),
-    Runs.load(ctx)
-  ]).then(function (results) {
-    return { strategy: results[0] || {}, auto: results[1] || {}, runs: results[2] || {} };
-  });
+  return Strategy.load(ctx);
 }
 function render(ctx) {
-  var coreCtx = strategyContext(ctx, ctx.data && ctx.data.strategy);
-  var node = Strategy.render(coreCtx);
-  var autoPanel = Auto.render(ctx, ctx.data && ctx.data.auto);
-  var runsPanel = Runs.render(ctx, ctx.data && ctx.data.runs);
-  if (node && node.insertBefore) {
-    var before = node.querySelector && node.querySelector('.z2m-subtabs');
-    node.insertBefore(autoPanel, before || null);
-    node.insertBefore(runsPanel, before || null);
-  } else if (node && node.appendChild) {
-    node.appendChild(autoPanel);
-    node.appendChild(runsPanel);
-  }
-  return node;
+  return Strategy.render(Object.assign({}, ctx, { data: ctx.data || {} }));
 }
 function mount(ctx) {
-  Strategy.mount(strategyContext(ctx, ctx.data && ctx.data.strategy, ctx.root));
+  Strategy.mount(Object.assign({}, ctx, { data: ctx.data || {}, root: ctx.root }));
 }
 function unmount(ctx) {
-  Strategy.unmount(strategyContext(ctx, ctx && ctx.data && ctx.data.strategy, ctx && ctx.root));
-  Auto.unmount();
-  Runs.unmount();
+  Strategy.unmount(Object.assign({}, ctx || {}, { data: ctx && ctx.data || {}, root: ctx && ctx.root }));
 }
-function createAdapter(api) { return Strategy.createAdapter ? Strategy.createAdapter(api) : null; }
+function createAdapter(api) {
+  return Strategy.createAdapter ? Strategy.createAdapter(api) : null;
+}
 
 return baseclass.extend({
   id: 'strategy',
   title: _('Стратегия'),
-  subtitle: _('Выбор, автоматический подбор и проверка способа обхода DPI'),
+  subtitle: _('Выбор и проверка способа обхода DPI'),
   load: load,
   render: render,
   mount: mount,
