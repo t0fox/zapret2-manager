6367c2a2154ba84abf3315552f43cc2962ea6f90
t0fox <t0fox@yandex.ru>
2026-09-03 22:05:09 +0300

    style: reduce DNS row button height

--- luci-app-zapret2-manager/files/www/luci-static/resources/view/zapret2-manager/z2m-shell.js
+++ luci-app-zapret2-manager/files/www/luci-static/resources/view/zapret2-manager/z2m-shell.js
@@ -7,7 +7,7 @@ var modalKeyHandler = null;
 
 function injectStylesheet(id, filename) {
   if (!document || !document.head) return;
-  var revision = '?v=dns-provider-editor-polish-20260903-r2';
+  var revision = '?v=dns-provider-editor-polish-20260903-r3';
   var existing = document.getElementById(id);
   if (existing) {
     var expected = L.resource('view/zapret2-manager/' + filename) + revision;
